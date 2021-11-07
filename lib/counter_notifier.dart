a8125ed56cc5dafd1270ff2470850d60fe3ed612
tokku5552 <physics.0121@gmail.com>
2021-11-07 21:55:07 +0900

    create counter_notifier

---
+++ lib/counter_notifier.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod_sample/count.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final counterProvider = StateNotifierProvider<CounterNotifier, Count>(
+  (ref) => CounterNotifier(const Count(0)),
+);
+
+class CounterNotifier extends StateNotifier<Count> {
+  CounterNotifier(Count state) : super(state);
+
+  void increment() {
+    state = state.copyWith(value: state.value + 1);
+  }
+}
