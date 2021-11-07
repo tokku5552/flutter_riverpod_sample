923971bb7791bee8f75e615febf7c20b5681c18b
tokku5552 <physics.0121@gmail.com>
2021-11-07 22:13:10 +0900

    wrapped runapp with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_sample/my_app.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
